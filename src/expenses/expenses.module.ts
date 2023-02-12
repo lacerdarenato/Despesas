36d547400eb83e64cd29b97398ac437848bc041b
Renato Lacerda <renato.lacerda@in8.com.br>
2023-02-11 22:19:21 -0300

    refact: remove useless code

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Expense } from './expense.entity';
 import { DatabaseModule } from 'src/database/database.module';
 import { expenseProviders } from './expense.provider';
 
 @Module({
   imports: [
-    // TypeOrmModule.forFeature([Expense]),
     DatabaseModule
   ],
   controllers: [ExpensesController],

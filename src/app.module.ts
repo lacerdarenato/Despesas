c1366954cf93155b099062ff766f6571f0e2a51f
Renato Lacerda <renato.lacerda@in8.com.br>
2023-02-12 09:35:42 -0300

    Refact: include env configuration on appmondule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,21 +10,39 @@ import { ExpensesModule } from './expenses/expenses.module';
 @Module({
   imports: [
     ExpensesModule,
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: process.env.DATABASE_URL,
-      port: Number(process.env.DATABASE_PORT),
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      synchronize: true,
-      logging: false,
-      entities: [Expense],
-      subscribers: [],
-      migrations: [],
-      autoLoadEntities: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('database.uri'),
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [Expense],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: "postgres",
+    //   host: process.env.DATABASE_URL,
+    //   port: Number(process.env.DATABASE_PORT),
+    //   username: process.env.DATABASE_USER,
+    //   password: process.env.DATABASE_PASSWORD,
+    //   database: process.env.POSTGRES_DB,
+    //   synchronize: true,
+    //   logging: false,
+    //   entities: [Expense],
+    //   subscribers: [],
+    //   migrations: [],
+    //   autoLoadEntities: true
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],

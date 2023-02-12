639e97ad6aa96df9e86defeac72e5cb7a4a19210
Renato Lacerda <renato.lacerda@in8.com.br>
2023-02-12 12:51:01 -0300

    refac: change path of dbModule to relative

--- src/expenses/expenses.module.ts
+++ src/expenses/expenses.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { expenseProviders } from './expense.provider';
 
 @Module({

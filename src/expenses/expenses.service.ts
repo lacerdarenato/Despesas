9c2e9d3a9b4f35e19e5ff7c3e98ec49cda1380bc
Renato Lacerda <renato.lacerda@in8.com.br>
2023-02-12 22:46:55 -0300

    feat: create getEmailByUserId on expense Service

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -3,10 +3,14 @@ import { Repository } from 'typeorm';
 import { CreateExpenseDto } from './dto/create-expense.dto';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
 import { Expense } from './expense.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class ExpensesService {
-  constructor(@Inject('EXPENSE_REPOSITORY') private expenseRepository: Repository<Expense>) { }
+  constructor(
+    @Inject('EXPENSE_REPOSITORY') private expenseRepository: Repository<Expense>,
+    @Inject('USER_REPOSITORY') private userRepository: Repository<User>
+  ) { }
 
   findAll(): Promise<Expense[]> {
     return this.expenseRepository.find();
@@ -18,7 +22,10 @@ export class ExpensesService {
 
   async create(createExpenseDto: CreateExpenseDto): Promise<Expense> {
     const newExpense = this.expenseRepository.create(createExpenseDto);
-    return await this.expenseRepository.save(newExpense);
+    const savedExpense = await this.expenseRepository.save(newExpense);
+    const userEmail = await this.getEmailByUserId(savedExpense)
+    console.log(userEmail)
+    return savedExpense;
   };
 
   async update(id: number, updateExpenseDto: UpdateExpenseDto): Promise<boolean> {
@@ -34,4 +41,10 @@ export class ExpensesService {
   async remove(id: number): Promise<void> {
     await this.expenseRepository.delete(id);
   }
+
+  async getEmailByUserId(expense: Expense): Promise<string> {
+    return (await this.userRepository.findOneBy(expense.user)).email;
+  }
+
+  
 }

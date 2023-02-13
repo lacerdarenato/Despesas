264e70922475d709f354f623c678de7781fbf5f0
Renato Lacerda <renato.lacerda@in8.com.br>
2023-02-13 00:59:25 -0300

    fix: include email env on configuration.ts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -9,5 +9,11 @@ export default () => ({
     },
     keys: {
         jwtSecretKey: process.env.JWT_SECRET_KEY.replace(/\\n/gm, '\n'),
-      },
+    },
+    email: {
+        host: process.env.EMAIL_HOST,
+        secure: process.env.EMAIL_SECURE_MODE,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    }
 });

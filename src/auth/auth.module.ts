97a0916d73119e749a8221fc5cdef540fe346885
Renato Lacerda <renato.lacerda@in8.com.br>
2023-02-13 22:48:02 -0300

    refact: separate auth and guard from login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,39 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
-import { DatabaseModule } from '../database/database.module';
-// import { LoggerModule } from '../logger/logger.module';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { JwtStrategy } from './strategy/jwt.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { User } from '../users/user.entity';
-import { userProviders } from '../users/user.provider';
+import { UsersModule } from '../users/users.module'
+import { PassportModule } from '@nestjs/passport/dist';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    PassportModule,
-    // LoggerModule,
-    TypeOrmModule.forFeature([User]),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        return {
-          secret: configService.get<string>('keys.jwtSecretKey'),
-          signOptions: { expiresIn: '60s' },
-        };
-      },
-      inject: [ConfigService],
-    }),
-  ],
-  providers: [
-    JwtStrategy,
-    AuthService,
-    ...userProviders,
-  ],
-  exports: [AuthService],
-  controllers: [AuthController],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule { }
